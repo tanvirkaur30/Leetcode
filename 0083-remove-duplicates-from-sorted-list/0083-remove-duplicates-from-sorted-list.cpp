d6df4b9a28f44f21bf0bea557a3274573365306c
Tanvir Kaur <239814455+tanvirkaur30@users.noreply.github.com>
2026-09-22 02:59:46 +0530

    Time: 0 ms (100%), Space: 16.4 MB (9.86%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -27,3 +27,22 @@ public:
         return head;
     }
 };
+/*class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = head;
+
+        while (temp != NULL && temp->next != NULL) {
+            if (temp->val == temp->next->val) {
+                ListNode* duplicate = temp->next;
+                temp->next = temp->next->next;
+                delete duplicate;
+            }
+            else {
+                temp = temp->next;
+            }
+        }
+
+        return head;
+    }
+};*/
