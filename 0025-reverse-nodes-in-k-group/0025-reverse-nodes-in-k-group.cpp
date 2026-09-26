1eb1ecd8c2dfd1ff2f516acfd24bdea57af2b402
Tanvir Kaur <239814455+tanvirkaur30@users.noreply.github.com>
2026-09-27 00:23:56 +0530

    Time: 0 ms (100%), Space: 16.5 MB (38.46%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -34,25 +34,25 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-       ListNode* temp=head;
-       ListNode* nextofkthnode=NULL;
-       ListNode* prevlast=NULL;
+       ListNode* temp=head;//temp will be the beginning of the current group
+       ListNode* nextofkthnode=NULL;//stores the first node of the next group before we cut the current 
+       ListNode* prevlast=NULL;//stores last node of previously reversed group
        while(temp!=NULL){
-        ListNode* kthnode = getkthNode(temp,k);
-        if(kthnode==NULL){
+        ListNode* kthnode = getkthNode(temp,k);//get kth node using temp not head 
+        if(kthnode==NULL){ //if no kthnode exists that means we have to connect the node as it is 
             if(prevlast) prevlast->next=temp;
             break;
         }
-        nextofkthnode=kthnode->next;
-        kthnode->next=NULL;
-        reverseLL(temp);
-        if(temp==head){
+        nextofkthnode=kthnode->next;//save beginning of the next group
+        kthnode->next=NULL;//cut the current group
+        reverseLL(temp);  //reverse it 
+        if(temp==head){  //if its the first group then head needs to be updated
             head=kthnode;
-        }else{
-            prevlast->next=kthnode;
+        }else{                     // if its the group after first group,
+            prevlast->next=kthnode; //connect the last node of previous group with first of next
         }
-        prevlast=temp;
-        temp=nextofkthnode;
+        prevlast=temp; // temp was the original first node, after reversal it becomes the LAST node.
+        temp=nextofkthnode;//move temp to the beginning of the next group
        }
        return head;
     }
